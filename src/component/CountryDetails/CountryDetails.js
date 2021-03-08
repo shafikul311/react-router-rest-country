d90334911a0b69a5b4a1e61d576656cdb2a4913d
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-03-08 23:13:40 +0600

    update

--- src/component/CountryDetails/CountryDetails.js
+++ src/component/CountryDetails/CountryDetails.js
@@ -1,69 +1,62 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-import { useParams } from 'react-router';
-import Card from 'react-bootstrap/Card';
-import { Table } from 'react-bootstrap';
-
+import { useParams } from "react-router";
+import Card from "react-bootstrap/Card";
+import { Table } from "react-bootstrap";
 
 const CountryDetails = () => {
-    const {alpha3Code} = useParams();
+  const { alpha3Code } = useParams();
 
-    const [countres , setCountries] = useState({})
-    
-    useEffect(() =>{
+  const [countres, setCountries] = useState({});
 
-        const url = `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`
+  useEffect(() => {
+    const url = `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`;
 
-       
-        fetch(url)
-        .then(response =>response.json())
-        .then(data =>setCountries(data))
-    },[])
-    const {name , capital} = countres
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  }, []);
+  const {
+    name,
+    capital,
+    flag,
+    population,
+    region,
+    timezones,
+  
+    area
+  
+  } = countres;
 
-    const countryStyle  = {
-        border : '1px solid green',
-        margin : '20px',
-        padding : '20px',
-        borderRadius : '25px',
-        textAlign : 'center'
-    }
+  const countryStyle = {
+      
+    // border: "1px solid green",
+    justifiedContent:'centre',
+    marginTop:'30px',
+    marginLeft:'35%',
+   
+    padding: "20px",
+    borderRadius: "25px",
+    textAlign: "center",
+  };
 
-    return (
-        <div style={countryStyle}>
-            <h2>country details here</h2>
-
-            <h1>{name}</h1>
-            <h1> capital : {capital}</h1>
-
-            <Table striped bordered hover>
-  <thead>
-    <tr>
-     
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
- <ul>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
- </ul>
-  </tbody>
-</Table>
-
-
-                          
-         
-        </div>
-        
-    );
+  return (
+    <div style={countryStyle}>
+      <Card style={{ width: "25rem" }}>
+        <Card.Img variant="top" src={flag} />
+        <Card.Body>
+          <Card.Title style={{fontSize:'40px'}}>{name}</Card.Title>
+          <Card.Title>Capital :{capital}</Card.Title>
+          <Card.Title>Population :{population} </Card.Title>
+          <Card.Title>Area : {area}</Card.Title>
+          <Card.Title>Region : {region}</Card.Title>
+          <Card.Title>Timezone :{timezones} </Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default CountryDetails;
